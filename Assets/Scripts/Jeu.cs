9a2253f3396d480cab4da44c8c810c28697bced5
Katelyn-Ellow <ellow7078@gmail.com>
2026-03-07 21:46:45 -0500

    Jeu-Olympic-Finale

--- Assets/Scripts/Jeu.cs
+++ Assets/Scripts/Jeu.cs
@@ -1,18 +1,86 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Jeu : MonoBehaviour
 {
+    //=====Variables publiques=======
+
+    //Temps minuterie
+    public TMP_Text textTemps;
+    float temps = 0;
+
+    public GameObject UIVictoire;
+    public GameObject UIDefaite;
+
+    //fin
+    bool jeuFini = false;
+
+    float delaiRedemarrage = 5f;
+    float tempsFin = 0;
+
+    //son
+    AudioSource musique;
+    
     void Start()
     {
+        //ne pas mettre les UI visibles
+        UIVictoire.SetActive(false);
+        UIDefaite.SetActive(false);
 
+        musique = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        //met le cronomettre, si perdu redemarre le jeu 
+        if (!jeuFini)
+        {
 
+            temps += Time.deltaTime;
+            //met le en letters, mais aussi enleve tout apres deciamls.
+            textTemps.text = $"Temps: " + temps.ToString("F1");
+        }
+        else 
+        {
+            if (Time.time > tempsFin + delaiRedemarrage)
+        {
+                if (Keyboard.current.spaceKey.wasPressedThisFrame)
+                {
+                    RedemarrerScene();
+                
+                }
+             }
+          }
+      }
+
+                 
+    ///=====Perdue=====
+    public void Defaite()
+    {
+        if (jeuFini) return;
+        jeuFini = true;
+
+        musique.Stop();
+        Debug.Log("LOSE!");
+        UIDefaite.SetActive(true);
+
+        tempsFin = Time.time;
     }
+
+    ///===Fin de jeu====
+    public void Victoire()
+    {
+        if (jeuFini) return;
+        jeuFini = true;
+
+        musique.Stop();
+        Debug.Log("Yippie!");
+        UIVictoire.SetActive(true);
+        tempsFin = Time.time;
+    }
+
 
     private void RedemarrerScene()
     {
--- Assets/Scripts/Skieur.cs
+++ Assets/Scripts/Skieur.cs
@@ -1,25 +1,124 @@
 using UnityEngine;
 using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class Skieur : MonoBehaviour
 {
+
+    //======== Variables publiques =======/
+
+    public float vitesse = 5f;
+
+    public InputAction onDeplacementVertical;
+    public InputAction onDeplacementHorizontal;
+
+
+    //Points
+    public int Points = 0;
+
+    //Sons
+    AudioSource sons;
+    public AudioClip sonPointage;
+    public AudioClip sonObstacles;
+
+    //UI
+    public TMP_Text PointageTexte;
+
+    //===== Variables privées
+    Rigidbody2D rigid;
+    Jeu game;
+
+
+    ///====== pas bas et haut (deplacement) ======
+    float deplacementHor = 0;
+    float deplacementVert = 0;
+
+   
     void Start()
     {
-
+        //fetch moi ses composants
+        rigid = GetComponent<Rigidbody2D>();
+        game = FindFirstObjectByType<Jeu>();
+        sons = GetComponent<AudioSource>();
     }
 
     // Utiliser ces fonctions pour activer et désactiver les InputActions
     private void OnEnable()
     {
-
+        onDeplacementHorizontal.Enable();
+        onDeplacementVertical.Enable();
     }
 
     private void OnDisable()
     {
-
+        onDeplacementHorizontal.Disable();
+        onDeplacementVertical.Disable();
     }
 
+    private void Update()
+    {
+        {
+    /// Pour les movements de skieur
+        deplacementHor = onDeplacementHorizontal.ReadValue<float>();
+        deplacementVert = onDeplacementVertical.ReadValue<float>();
 
+        if(deplacementVert > 0)
+        {
+            deplacementVert = 0;
+        }
+       
+        // Definir la vélocité linéaire de skieur.
+        rigid.linearVelocity = new Vector2(
+            deplacementHor * vitesse,
+            deplacementVert * vitesse
+            );
+    }}
+
+    //une methode OnCollisionEnter2D (collison entre sapins) + collision de Yeti
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Obstacle"))
+        {
+            rigid.linearVelocity = Vector2.zero;
+            Debug.Log("Toucher!");
+            sons.PlayOneShot(sonObstacles);
+        }
+        
+        //collison de Yeti + UI
+        if(collision.gameObject.tag == ("Danger"))
+        {
+            rigid.linearVelocity = Vector2.zero;
+            rigid.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            game.Defaite();
+        }
+    }
+
+    //une methode OnTriggerEnter2D (bon-homme de neige)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == ("Points"))
+        {
+            collision.gameObject.SetActive(false);
+            //Ajouter les points
+            Points += 1;
+            //TEXT UI
+            PointageTexte.text = $"Points : {Points}";
+            //ding! son
+            sons.PlayOneShot(sonPointage);
+        }
+
+        //trigger collision finish
+        if (collision.gameObject.tag == ("Finish"))
+        {
+            rigid.linearVelocity = Vector2.zero;
+            rigid.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            game.Victoire();
+
+        }
+    }
 
     // Il faut appeller cette fonction dans la collision avec le yéti.
     void DeconnecterCamera()
